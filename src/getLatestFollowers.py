d279d91257f16b8f197e17dcb8f6cdb16792bf50
VoidSon-Stubs <jashp3534@gmail.com>
2024-09-02 22:12:58 +0530

    Update getLatestFollowers.py

--- src/getLatestFollowers.py
+++ src/getLatestFollowers.py
@@ -3,80 +3,62 @@ import json
 import sys
 import re
 
-def get_latest_followers(handle, token, readme_path):
+if __name__ == "__main__":
+    assert len(sys.argv) == 4
+    handle = sys.argv[1]
+    token = sys.argv[2]
+    readmePath = sys.argv[3]
+
     headers = {
         "Authorization": f"token {token}"
     }
 
-    followers = []
-    cursor = None
+    query = '''
+    query {
+        user(login: "%s") {
+            followers(last: 12) {
+                nodes {
+                    login
+                    avatarUrl
+                }
+            }
+        }
+    }
+    ''' % handle
 
-    while True:
-        query = f'''
-        query {{
-            user(login: "{handle}") {{
-                followers(first: 12{f', after: "{cursor}"' if cursor else ''}) {{
-                    pageInfo {{
-                        endCursor
-                        hasNextPage
-                    }}
-                    nodes {{
-                        login
-                        name
-                        databaseId
-                    }}
-                }}
-            }}
-        }}
+    response = requests.post(
+        "https://api.github.com/graphql",
+        json.dumps({"query": query}),
+        headers=headers
+    )
+
+    if not response.ok or "data" not in response.json():
+        print("Failed to fetch data from GitHub API")
+        sys.exit(1)
+
+    followers_data = response.json()["data"]["user"]["followers"]["nodes"]
+
+    html = "<table>\n<tr>\n"
+    for follower in followers_data:
+        html += f'''
+        <td align="center">
+            <a href="https://github.com/{follower['login']}">
+                <img src="{follower['avatarUrl']}" width="100px;" alt="{follower['login']}"/>
+            </a>
+            <br />
+            <a href="https://github.com/{follower['login']}">{follower['login']}</a>
+        </td>
         '''
-        response = requests.post("https://api.github.com/graphql", json={"query": query}, headers=headers)
-        if not response.ok or "data" not in response.json():
-            print("Failed to fetch data from GitHub API")
-            exit(1)
-        
-        res = response.json()["data"]["user"]["followers"]
-        followers.extend(res["nodes"])
-        if not res["pageInfo"]["hasNextPage"] or len(followers) >= 12:
-            break
-        cursor = res["pageInfo"]["endCursor"]
+    html += "\n</tr>\n</table>"
 
-    followers = followers[:12]  # Get the latest 12 followers
+    with open(readmePath, "r") as readme_file:
+        content = readme_file.read()
 
-    html = "<table>\n"
-    for i, follower in enumerate(followers):
-        login = follower["login"]
-        name = follower["name"] if follower["name"] else login
-        id = follower["databaseId"]
-        if i % 4 == 0:
-            if i != 0:
-                html += "  </tr>\n"
-            html += "  <tr>\n"
-        html += f'''    <td align="center">
-      <a href="https://github.com/{login}">
-        <img src="https://avatars.githubusercontent.com/u/{id}" width="100px;" alt="{login}"/>
-      </a>
-      <br />
-      <a href="https://github.com/{login}">{name}</a>
-    </td>
-'''
+    new_content = re.sub(
+        r"(?<=<!--START_SECTION:latest-followers-->)[\s\S]*(?=<!--END_SECTION:latest-followers-->)",
+        f"\n{html}\n",
+        content
+    )
 
-    html += "  </tr>\n</table>"
-
-    with open(readme_path, "r") as readme:
-        content = readme.read()
-
-    new_content = re.sub(r"(?<=<!--START_SECTION:latest-followers-->)[\s\S]*(?=<!--END_SECTION:latest-followers-->)", f"\n{html}\n", content)
-
-    with open(readme_path, "w") as readme:
-        readme.write(new_content)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python getLatestFollowers.py <GitHub_Username> <GitHub_Token> <README_File_Path>")
-        exit(1)
-
-    github_username = sys.argv[1]
-    github_token = sys.argv[2]
-    readme_file_path = sys.argv[3]
-
-    get_latest_followers(github_username, github_token, readme_file_path)
+    with open(readmePath, "w") as readme_file:
+        readme_file.write(new_content)
